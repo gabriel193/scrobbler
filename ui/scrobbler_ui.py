971106c69f5d830942d37d31ff03fff71beacac0
Gabriel <gabrielalencar0531@gmail.com>
2024-10-22 15:14:50 -0300

    campos para API Key e API Secret, além de um link clicável para o usuário gerar essas credenciais

--- ui/scrobbler_ui.py
+++ ui/scrobbler_ui.py
@@ -1,11 +1,11 @@
-#scrobbler_ui.py
+# scrobbler_ui.py
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(300, 350)  # Ajuste no tamanho para tornar o app mais compacto
+        MainWindow.resize(300, 500)  # Ajuste no tamanho para acomodar os novos campos
         MainWindow.setStyleSheet("""
             QWidget {
                 background-color: #f0f0f0;
@@ -46,15 +46,15 @@ class Ui_MainWindow(object):
 
         # Layout principal vertical
         self.main_layout = QtWidgets.QVBoxLayout(self.centralwidget)
-        self.main_layout.setContentsMargins(10, 10, 10, 10)  # Reduz as margens superior e inferior
-        self.main_layout.setSpacing(5)  # Reduz o espaçamento entre os widgets
+        self.main_layout.setContentsMargins(10, 10, 10, 10)
+        self.main_layout.setSpacing(5)
 
         # Layout do usuário
         username_layout = QtWidgets.QHBoxLayout()
         username_layout.setSpacing(5)
         self.usernameLabel = QtWidgets.QLabel("Usuário")
         self.usernameInput = QtWidgets.QLineEdit()
-        self.usernameInput.setPlaceholderText("Coloque aqui seu usuário")  # Texto de exemplo
+        self.usernameInput.setPlaceholderText("Coloque aqui seu usuário")
         username_layout.addWidget(self.usernameLabel)
         username_layout.addWidget(self.usernameInput)
         self.main_layout.addLayout(username_layout)
@@ -65,10 +65,35 @@ class Ui_MainWindow(object):
         self.passwordLabel = QtWidgets.QLabel("Senha")
         self.passwordInput = QtWidgets.QLineEdit()
         self.passwordInput.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.passwordInput.setPlaceholderText("Coloque aqui sua senha")  # Texto de exemplo
+        self.passwordInput.setPlaceholderText("Coloque aqui sua senha")
         password_layout.addWidget(self.passwordLabel)
         password_layout.addWidget(self.passwordInput)
         self.main_layout.addLayout(password_layout)
+
+        # Campos para API Key e API Secret
+        api_key_layout = QtWidgets.QHBoxLayout()
+        api_key_layout.setSpacing(5)
+        self.apiKeyLabel = QtWidgets.QLabel("API Key")
+        self.apiKeyInput = QtWidgets.QLineEdit()
+        self.apiKeyInput.setPlaceholderText("Coloque sua API Key")
+        api_key_layout.addWidget(self.apiKeyLabel)
+        api_key_layout.addWidget(self.apiKeyInput)
+        self.main_layout.addLayout(api_key_layout)
+
+        api_secret_layout = QtWidgets.QHBoxLayout()
+        api_secret_layout.setSpacing(5)
+        self.apiSecretLabel = QtWidgets.QLabel("API Secret")
+        self.apiSecretInput = QtWidgets.QLineEdit()
+        self.apiSecretInput.setPlaceholderText("Coloque seu API Secret")
+        api_secret_layout.addWidget(self.apiSecretLabel)
+        api_secret_layout.addWidget(self.apiSecretInput)
+        self.main_layout.addLayout(api_secret_layout)
+
+        # Link para gerar as credenciais
+        self.apiLinkLabel = QtWidgets.QLabel()
+        self.apiLinkLabel.setText('<a href="https://www.last.fm/api/account/create">Clique aqui para gerar suas credenciais de API</a>')
+        self.apiLinkLabel.setOpenExternalLinks(True)
+        self.main_layout.addWidget(self.apiLinkLabel)
 
         # Botão de login
         self.loginButton = QtWidgets.QPushButton("Login")
@@ -86,7 +111,7 @@ class Ui_MainWindow(object):
         artist_layout.setSpacing(5)
         self.artistLabel = QtWidgets.QLabel("Artista")
         self.artistInput = QtWidgets.QLineEdit()
-        self.artistInput.setPlaceholderText("Coloque aqui o nome do artista")  # Texto de exemplo
+        self.artistInput.setPlaceholderText("Coloque aqui o nome do artista")
         artist_layout.addWidget(self.artistLabel)
         artist_layout.addWidget(self.artistInput)
         self.main_layout.addLayout(artist_layout)
@@ -96,7 +121,7 @@ class Ui_MainWindow(object):
         song_layout.setSpacing(5)
         self.songLabel = QtWidgets.QLabel("Música")
         self.songInput = QtWidgets.QLineEdit()
-        self.songInput.setPlaceholderText("Coloque aqui o nome da música")  # Texto de exemplo
+        self.songInput.setPlaceholderText("Coloque aqui o nome da música")
         song_layout.addWidget(self.songLabel)
         song_layout.addWidget(self.songInput)
         self.main_layout.addLayout(song_layout)
@@ -106,12 +131,9 @@ class Ui_MainWindow(object):
         quantity_layout.setSpacing(5)
         self.quantityLabel = QtWidgets.QLabel("Quantidade")
         self.quantityInput = QtWidgets.QLineEdit()
-        self.quantityInput.setPlaceholderText("Coloque aqui a quantidade de scrobbles")  # Texto de exemplo
-        
-        # Configura um validador que só permite números maiores que 0
-        int_validator = QtGui.QIntValidator(1, 9999)  # Limita os valores de 1 a 9999, por exemplo
+        self.quantityInput.setPlaceholderText("Coloque aqui a quantidade de scrobbles")
+        int_validator = QtGui.QIntValidator(1, 9999)
         self.quantityInput.setValidator(int_validator)
-        
         quantity_layout.addWidget(self.quantityLabel)
         quantity_layout.addWidget(self.quantityInput)
         self.main_layout.addLayout(quantity_layout)
@@ -128,31 +150,21 @@ class Ui_MainWindow(object):
         buttons_layout.addWidget(self.modeToggle)
         self.main_layout.addLayout(buttons_layout)
 
-        # Barra de progresso logo abaixo dos botões
+        # Barra de progresso
         self.progressBar = QtWidgets.QProgressBar()
         self.progressBar.setMaximum(100)
         self.progressBar.setValue(0)
         self.main_layout.addWidget(self.progressBar)
 
-        # Layout horizontal para a imagem do usuário e o statusLabel
-        status_layout = QtWidgets.QHBoxLayout()
-        status_layout.setSpacing(5)
-
-        # Criar o userImageLabel
-        self.userImageLabel = QtWidgets.QLabel()
-        self.userImageLabel.setFixedSize(32, 32)
-        self.userImageLabel.setVisible(False)
-        status_layout.addWidget(self.userImageLabel, 0)  # Stretch fator 0 para a imagem
-
-        # Botões de Pausar e Cancelar em um layout horizontal
+        # Botões de Pausar e Cancelar
         control_buttons_layout = QtWidgets.QHBoxLayout()
         control_buttons_layout.setSpacing(5)
         self.pauseButton = QtWidgets.QPushButton("Pausar")
         self.pauseButton.setFixedHeight(30)
-        self.pauseButton.setVisible(False)  # Inicialmente oculto
+        self.pauseButton.setVisible(False)
         self.cancelButton = QtWidgets.QPushButton("Cancelar")
         self.cancelButton.setFixedHeight(30)
-        self.cancelButton.setVisible(False)  # Inicialmente oculto
+        self.cancelButton.setVisible(False)
         control_buttons_layout.addWidget(self.pauseButton)
         control_buttons_layout.addWidget(self.cancelButton)
         self.main_layout.addLayout(control_buttons_layout)
@@ -162,9 +174,7 @@ class Ui_MainWindow(object):
         self.statusLabel.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
         self.statusLabel.setWordWrap(True)
         self.statusLabel.setStyleSheet("font-weight: bold; color: #333333;font-size: 10px;")
-        status_layout.addWidget(self.statusLabel, 1)  # Stretch fator 1 para o texto
-
-        self.main_layout.addLayout(status_layout)
+        self.main_layout.addWidget(self.statusLabel)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.retranslateUi(MainWindow)
